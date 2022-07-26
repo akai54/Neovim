94b3ae69be5a6a56a1359a0660eb8d3fe2d339f3
beshoux <beshoux@outlook.fr>
2022-07-26 21:33:10 +0200

    Clearing up.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -13,6 +13,5 @@ vim.g.onedarker_italic_conditionals = false
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  -- vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end

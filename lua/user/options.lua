f54bbd016e9aace90c7d0a1f57e90c557c3ef93a
beshoux <beshoux@outlook.fr>
2022-07-27 05:27:00 +0200

    Enabled wsl clipboard.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -50,18 +50,18 @@ for k, v in pairs(options) do
 end
 
 -- Enable clipboard in WSL --
---[[ vim.g.clipboard = {
-	name = "win32yank-wsl",
-	copy = {
-		["+"] = "win32yank.exe -i --crlf",
-		["*"] = "win32yank.exe -i --crlf",
-	},
-	paste = {
-		["+"] = "win32yank.exe -o --lf",
-		["*"] = "win32yank.exe -o --lf",
-	},
-	cache_enabled = false,
-} ]]
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = false,
+}
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]

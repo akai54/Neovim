2cd21ebe601e693bb0e162033756982840ccc1e1
besher addam <beshoux@outlook.fr>
2022-08-10 05:13:48 +0200

    Added harpoon into Whichkey.

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -89,13 +89,18 @@ local mappings = {
   ["w"] = { "<cmd>w!<CR>", "Save" },
   ["q"] = { "<cmd>q!<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
-  ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
     "Find files",
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+
+  h = {
+    name = "Harpoon",
+    a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add" },
+    m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Menu" },
+  },
 
   p = {
     name = "Packer",

39de5315e93c24240578d3c9c78842bdff40f217
beshoux <beshoux@outlook.fr>
2022-12-16 15:47:07 +0100

    Fix comments plugin partly.

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -3,47 +3,28 @@ if not status_ok then
   return
 end
 
-local status_ok_1, _ = pcall(require, "lsp-inlayhints")
-if not status_ok_1 then
-  return
-end
-
 comment.setup {
+  ---LHS of toggle mappings in NORMAL mode
   toggler = {
+    ---Line-comment toggle keymap
     line = "gcc",
+    ---Block-comment toggle keymap
     block = "gbc",
   },
+  ---LHS of operator-pending mappings in NORMAL and VISUAL mode
   opleader = {
+    ---Line-comment keymap
     line = "gc",
+    ---Block-comment keymap
     block = "gb",
   },
-  ignore = "^$",
-  pre_hook = function(ctx)
-    -- For inlay hints
-    local line_start = (ctx.srow or ctx.range.srow) - 1
-    local line_end = ctx.erow or ctx.range.erow
-    require("lsp-inlayhints.core").clear(0, line_start, line_end)
-
-    require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-
-    if vim.bo.filetype == "javascript" or vim.bo.filetype == "typescript" then
-      local U = require "Comment.utils"
-
-      -- Determine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.blockwise then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = type,
-        location = location,
-      }
-    end
-  end,
+  ---LHS of extra mappings
+  extra = {
+    ---Add comment on the line above
+    above = "gcO",
+    ---Add comment on the line below
+    below = "gco",
+    ---Add comment at the end of line
+    eol = "gcA",
+  },
 }

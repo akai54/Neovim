d5381ef0c28d1da303c91f6aae7c4eec46f00371
besher <beshoux@outlook.fr>
2023-02-20 22:38:38 +0100

    config better_escape.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -241,6 +241,9 @@ return packer.startup(function(use)
   -- TailwindCSS colorizer
   use "roobert/tailwindcss-colorizer-cmp.nvim"
 
+  -- Escape different Nvim modes
+  use "max397574/better-escape.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

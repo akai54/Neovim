d166e6063cff04dc336946a40308d6a0b7bca5b6
besher <beshoux@outlook.fr>
2023-02-20 14:25:14 +0100

    Add ruff and lua lsp.

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,54 +1,54 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+    return
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml", "solidity" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
-    formatting.black.with { extra_args = { "--fast" } },
+    debug = false,
+    sources = {
+        formatting.prettier.with {
+            extra_filetypes = { "toml", "solidity" },
+            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+        },
+        formatting.ruff,
 
-    formatting.stylua,
-    formatting.shfmt,
-    diagnostics.shellcheck,
-    diagnostics.selene,
-    diagnostics.zsh,
-    diagnostics.flake8.with { extra_args = { "--extend-ignore E266,E302,E401", "--max-line-length=90" } },
-  },
+        formatting.stylua,
+        formatting.shfmt,
+        diagnostics.shellcheck,
+        diagnostics.selene,
+        diagnostics.zsh,
+        diagnostics.ruff,
+    },
 }
 
 local unwrap = {
-  method = null_ls.methods.DIAGNOSTICS,
-  filetypes = { "rust" },
-  generator = {
-    fn = function(params)
-      local diagnostics_2 = {}
-      -- sources have access to a params object
-      -- containing info about the current file and editor state
-      for i, line in ipairs(params.content) do
-        local col, end_col = line:find "unwrap()"
-        if col and end_col then
-          -- null-ls fills in undefined positions
-          -- and converts source diagnostics into the required format
-          table.insert(diagnostics_2, {
-            row = i,
-            col = col,
-            end_col = end_col,
-            source = "unwrap",
-            message = "hey " .. os.getenv "USER" .. ", don't forget to handle this",
-            severity = 2,
-          })
-        end
-      end
-      return diagnostics_2
-    end,
-  },
+    method = null_ls.methods.DIAGNOSTICS,
+    filetypes = { "rust" },
+    generator = {
+        fn = function(params)
+            local diagnostics_2 = {}
+            -- sources have access to a params object
+            -- containing info about the current file and editor state
+            for i, line in ipairs(params.content) do
+                local col, end_col = line:find "unwrap()"
+                if col and end_col then
+                    -- null-ls fills in undefined positions
+                    -- and converts source diagnostics into the required format
+                    table.insert(diagnostics_2, {
+                        row = i,
+                        col = col,
+                        end_col = end_col,
+                        source = "unwrap",
+                        message = "hey " .. os.getenv "USER" .. ", don't forget to handle this",
+                        severity = 2,
+                    })
+                end
+            end
+            return diagnostics_2
+        end,
+    },
 }
 null_ls.register(unwrap)
